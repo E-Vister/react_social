c77b4cec3c2515e43002c85ec3cea7ad664c9e3b
E-Vister <blitz.vister@gmail.com>
2022-09-10 22:14:57 +0300

    repository init

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+                alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <div className="nav-button"><a href='#'>Profile</a></div>
+            <div className="nav-button"><a href='##'>Messages</a></div>
+            <div className="nav-button"><a href='###'>News</a></div>
+            <div className="nav-button"><a href='####'>Music</a></div>
+            <br/>
+            <div className="nav-button"><a href='#####'>Settings</a></div>
+        </nav>
+    );
+}
+
+export default Navbar;
