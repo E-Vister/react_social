8a2d5f37445b6cfd775db3e84a9c0124a262a9df
E-Vister <blitz.vister@gmail.com>
2022-09-11 20:58:55 +0300

    Replaced css with sass. Styles are placed in modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import scss from './Header.module.scss';
+
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={scss.header}>
             <img
+                className={scss.logo}
                 src="https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
                 alt="logo"/>
         </header>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
+import scss from './Navbar.module.scss';
+
 const Navbar = () => {
     return (
-        <nav className='navbar'>
-            <div className="nav-button"><a href='#'>Profile</a></div>
-            <div className="nav-button"><a href='##'>Messages</a></div>
-            <div className="nav-button"><a href='###'>News</a></div>
-            <div className="nav-button"><a href='####'>Music</a></div>
+        <nav className={scss.navbar}>
+            <div className={scss.item}><a href='#'>Profile</a></div>
+            <div className={scss.item}><a href='#'>Messages</a></div>
+            <div className={scss.item}><a href='#'>News</a></div>
+            <div className={scss.item}><a href='#'>Music</a></div>
             <br/>
-            <div className="nav-button"><a href='#####'>Settings</a></div>
+            <div className={scss.item}><a href='#'>Settings</a></div>
         </nav>
     );
 }
