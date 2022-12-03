1c1ec5a50c4cc3460e5d442a8c52c91b2925a784
E-Vister <blitz.vister@gmail.com>
2022-12-03 23:40:40 +0300

    Update messages page layout

--- src/components/Content/Messages/DialogItem/DialogItem.jsx
+++ src/components/Content/Messages/DialogItem/DialogItem.jsx
@@ -1,15 +1,27 @@
 import scss from './DialogItem.module.scss';
 import {NavLink} from "react-router-dom";
+
 const DialogItem = (props) => {
     let path = `/messages/${props.id}`;
 
     return (
-        <div className={scss.container}>
-            <NavLink to={path}>
-                <img className={scss.avatar} src={props.avatar} alt="avatar"/>
-                {props.username}
-            </NavLink>
-        </div>
+        <NavLink className={scss.container} to={path}>
+            <div className={scss.wrapper}>
+                <div className={scss.avatar}>
+                    <img className={scss.avatar_image} src={props.avatar} alt="avatar"/>
+                </div>
+                <div className={scss.title}>
+                    <div className={scss.username}>{props.username}</div>
+                    <div className={scss.last_message}>
+                        {(props.lastMessageAvatar) ? <img className={scss.avatar_image}
+                             src={props.lastMessageAvatar}
+                             alt="last_message_avatar"
+                        /> : ''}
+                        {props.lastMessage}
+                    </div>
+                </div>
+            </div>
+        </NavLink>
     );
 }
 
--- src/components/Content/Messages/MessagesContainer.jsx
+++ src/components/Content/Messages/MessagesContainer.jsx
@@ -8,14 +8,25 @@ import {compose} from "redux";
 
 let mapStateToProps = (state) => {
     return {
-        dialogsElements: state.dialogs.dialogsArray.map((item) => {
+        dialogsElements: state.dialogs.dialogsArray.map((item, index, array) => {
             const [interlocutorData] = item.members.filter(i => i.id !== state.auth.userId);
+            const [currentUserData] = item.members.filter(i => i.id === state.auth.userId);
+
+            const [lastMessageObject] = item.messages.slice(-1);
+
+            const lastMessageContent = lastMessageObject.content;
+            const lastMessageAvatar = (lastMessageObject.authorId === state.auth.userId)
+                ? currentUserData.avatar
+                : false;
 
             return <DialogItem
                 username={`${interlocutorData.name} ${interlocutorData.surname}`}
                 avatar={interlocutorData.avatar}
                 key={interlocutorData.id}
-                id={item.dialogId}/>
+                id={item.dialogId}
+                lastMessage={lastMessageContent}
+                lastMessageAvatar={lastMessageAvatar}
+            />
         }),
         dialogsPage: state.dialogs.dialogsArray.map((item) => {
             return <Route path={`/${item.dialogId}/`}
